8496ab6cbae5fc3a839bc63fc8ecfe8b7b21f15d
Aybonya <aiba13792008@gmail.com>
2026-03-19 09:32:46 +0500

    update main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("Hello World!")
+def bubble_sort(numbers):
+    n = len(numbers)
+
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if numbers[j] > numbers[j + 1]:
+                numbers[j], numbers[j + 1] = numbers[j + 1], numbers[j]
+
+    return numbers
+
+
+values = [5, 2, 9, 1, 3]
+sorted_values = bubble_sort(values)
+
+print("Sorted list:", sorted_values)
